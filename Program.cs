3d74a9a2615fe69311adf8e8bafc59ad58374d6f
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-24 09:51:09 -0300

    set db

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 global using rpg_api.Models;
+using Microsoft.EntityFrameworkCore;
+using rpg_api.Data;
 using rpg_api.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(opt =>
+//         opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DataContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
