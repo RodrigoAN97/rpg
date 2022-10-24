69428553a4a82ee81c4fe9802f75fc50c59579e7
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-24 15:59:56 -0300

    register db

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rpg_api.Data;
+using rpg_api.Dtos.Character.User;
+
+namespace rpg_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(new User{ UserName = request.Username }, request.Password);
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
