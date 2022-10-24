fe81a6d822535576365522b97c331d090ca5fd41
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-24 13:47:39 -0300

    cleanup

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ using rpg_api.Services.CharacterService;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(opt =>
-//         opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<DataContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
         
