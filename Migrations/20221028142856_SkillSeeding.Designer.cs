8fb68ed4b11ba94cd5bba7188acf477f656f9c20
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-28 11:31:34 -0300

    skill seeding

--- Migrations/20221028142856_SkillSeeding.Designer.cs
+++ Migrations/20221028142856_SkillSeeding.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using rpg_api.Data;
@@ -11,9 +12,10 @@ using rpg_api.Data;
 namespace rpg_api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221028142856_SkillSeeding")]
+    partial class SkillSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
