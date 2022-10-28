e7342ed0c1208a607eae012295ab689fb38776d8
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-28 10:55:30 -0300

    skill

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -16,5 +16,6 @@ namespace rpg_api.Data
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Weapon> Weapons => Set<Weapon>();
+        public DbSet<Skill> Skills => Set<Skill>();
     }
 }
--- Models/Character.cs
+++ Models/Character.cs
@@ -16,5 +16,6 @@ namespace rpg_api.Models
         public RpgClass Class { get; set; } = RpgClass.Knight;
         public User User { get; set; } = new User();
         public Weapon? Weapon { get; set; }
+        public List<Skill>? Skills { get; set; }
     }
 }
