60473373f949507b4ed5cfc3fb41d5416ec29a0c
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-29 10:18:14 -0300

    add fight

--- Migrations/20221029131754_FightProperties.Designer.cs
+++ Migrations/20221029131754_FightProperties.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using rpg_api.Data;
@@ -11,9 +12,10 @@ using rpg_api.Data;
 namespace rpg_api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221029131754_FightProperties")]
+    partial class FightProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
