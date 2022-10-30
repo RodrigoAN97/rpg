09d7f68cf8007db3537cb8043a15b8898c7cc46a
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-10-30 11:23:51 -0300

    get characters in the method

--- Services/FightService/FightService.cs
+++ Services/FightService/FightService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using rpg_api.Data;
 using rpg_api.Dtos.Fight;
 using rpg_api.Services.CharacterService;
@@ -30,16 +31,22 @@ namespace rpg_api.Services.FightService
         {
             var response = new ServiceResponse<AttackResultDto>();
             try {
-                var attacker = await _characterService.GetCharacterById(request.AttackerId);
+                var attacker = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .Include(c => c.Skills)
+                    .FirstOrDefaultAsync(c => c.Id == request.AttackerId && c.User.Id == _globalService.GetUserId());
 
-                var opponent = await _globalService.GetOpponentCharacterById(request.OpponentId);
+                var opponent = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .Include(c => c.Skills)
+                    .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
 
-                if(attacker.Data == null || opponent.Data == null){
+                if(attacker == null || opponent == null){
                     response.Success = false;
                     response.Message = "Character not found";
                     return response;
                 }
-                if(attacker.Data.Weapon == null){
+                if(attacker.Weapon == null){
                     response.Success = false;
                     response.Message = "The attacker doesn't have a weapon";
                     return response;
@@ -47,21 +54,21 @@ namespace rpg_api.Services.FightService
 
                 // TODO: write another method to calculate the damage, use intelligence or delete this prop
                 //TODO: save fights, defeats and victories to the characters context
-                int damage = attacker.Data.Weapon.Damage + (new Random().Next(attacker.Data.Strength));
-                damage -= new Random().Next(opponent.Data.Defense);
+                int damage = attacker.Weapon.Damage + new Random().Next(attacker.Strength) + new Random().Next(attacker.Intelligence);
+                damage -= new Random().Next(opponent.Defense);
                 if(damage > 0){
-                    opponent.Data.HitPoints -= damage;
+                    opponent.HitPoints -= damage;
                 }
-                if(opponent.Data.HitPoints < 0){
-                    response.Message = $"{opponent.Data.Name} has been defeated!";
+                if(opponent.HitPoints < 0){
+                    response.Message = $"{opponent.Name} has been defeated!";
                 }
                 await _context.SaveChangesAsync();
 
                 response.Data = new AttackResultDto {
-                    AttackerName = attacker.Data.Name,
-                    OpponentName = opponent.Data.Name,
-                    AttackerHP = attacker.Data.HitPoints,
-                    OpponentHP = opponent.Data.HitPoints,
+                    AttackerName = attacker.Name,
+                    OpponentName = opponent.Name,
+                    AttackerHP = attacker.HitPoints,
+                    OpponentHP = opponent.HitPoints,
                     Damage = damage
                 };
                 return response;
