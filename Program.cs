b67f2316d25d4985c682623862dbcd83969e02f8
RodrigoAN97 <rodrigo_neves97@outlook.com>
2023-02-18 15:50:32 -0300

    swagger

--- Program.cs
+++ Program.cs
@@ -54,7 +54,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "rpg");
+        c.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
